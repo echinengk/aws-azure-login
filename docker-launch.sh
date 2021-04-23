24192f46f4bd9890b9f419043f80a922f24b1ce6
Elvis Chinen da Silva <elvis.silva@brlink.com.br>
2021-04-23 16:28:10 -0300

    ajuste no script docker-launch para funcionamento correto no meu environment

--- docker-launch.sh
+++ docker-launch.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+HISTCONTROL=ignoreboth
 
-docker run --rm -it -v ~/.aws:/root/.aws dtjohnson/aws-azure-login "$@"
+docker run --rm -it -v /Users/elvis.silva/iCloudDrive/bkp/aws:/root/.aws --env AZURE_DEFAULT_PASSWORD='SENHA' sportradar/aws-azure-login "$@"
